0be58bde0aa9073f277fe5e3610b1a8fb4147559
Andrew Mleczko <andrew@mleczko.net>
2012-06-12 10:23:22 +0200

    add isHomePage

--- src/restarter/policy/browser/configurations.py
+++ src/restarter/policy/browser/configurations.py
@@ -2,6 +2,7 @@
 
 from Products.Five import BrowserView
 from zope.interface import implements
+from zope.component import getMultiAdapter
 
 from restarter.policy.interfaces import IRestarterConfiguration
 
@@ -11,4 +12,6 @@ class RestarterConfig(BrowserView):
     implements(IRestarterConfiguration)
 
     def isHomePage(self):
-        return True
+        context_state = getMultiAdapter((self.context, self.request), name='plone_context_state')
+        portal_state = getMultiAdapter((self.context, self.request), name='plone_portal_state')
+        return portal_state.portal_url() == context_state.canonical_object_url()
