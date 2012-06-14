d895ef0e78763e8b7b36e64cec388278416d3fd8
Andrew Mleczko <andrew@mleczko.net>
2012-06-14 10:33:43 +0200

    minimize upgrade steps

--- src/restarter/policy/upgrades.py
+++ src/restarter/policy/upgrades.py
@@ -1,49 +1,14 @@
-import logging
 from Products.CMFCore.utils import getToolByName
 
 PROFILE_ID='profile-restarter.policy:default'
 
 
-def add_assc_roles(context, logger=None):
-    if logger is None:
-        # Called as upgrade step: define our own logger.
-        logger = logging.getLogger('restarter.policy')
-
-    setup = getToolByName(context, 'portal_setup')
-    setup.runImportStepFromProfile(PROFILE_ID, 'rolemap')
-    logger.info("New assoc role added.")
-
-def add_story_ct(context, logger=None):
-    if logger is None:
-        # Called as upgrade step: define our own logger.
-        logger = logging.getLogger('restarter.policy')
-
-    setup = getToolByName(context, 'portal_setup')
-    setup.runImportStepFromProfile(PROFILE_ID, 'typeinfo')
-    logger.info("CompanyStory added.")
-
-def add_employee_role(context, logger=None):
-    if logger is None:
-        # Called as upgrade step: define our own logger.
-        logger = logging.getLogger('restarter.policy')
-
-    setup = getToolByName(context, 'portal_setup')
-    setup.runImportStepFromProfile(PROFILE_ID, 'rolemap')
-    logger.info("New employee role added.")
-
+def add_employee_idx(context):
     catalog = getToolByName(context, 'portal_catalog')
     catalog.addIndex('company_employees', 'KeywordIndex')
 
 
-def upgrade_0004_to_0005(setuptool, logger=None):
-    setuptool.runAllImportStepsFromProfile('profile-restarter.policy:upgrade_0004_to_0005')
-
-
-def upgrade_0005_to_0006(context, logger=None):
-    context.runAllImportStepsFromProfile('profile-restarter.policy:upgrade_0005_to_0006')
-
-
-def upgrade_0006_to_0007(context, logger=None):
+def add_company_idx(context):
     catalog = getToolByName(context, 'portal_catalog')
     catalog.addIndex('city', 'FieldIndex')
     catalog.addIndex('province', 'KeywordIndex')
@@ -52,6 +17,37 @@ def upgrade_0006_to_0007(context, logger=None):
     catalog.addIndex('company_sectore', 'KeywordIndex')
 
 
-def upgrade_0007_to_0008(context, logger=None):
-    context.runAllImportStepsFromProfile('profile-restarter.policy:upgrade_0007_to_0008')
+def setupInitialStructure(portal):
+    if 'aziende' not in portal.objectIds() and 'Aziende' not in portal.objectIds():
+        companies = portal[portal.invokeFactory('Companies','aziende')]
+        companies.setTitle(u'Aziende')
+    if 'notizie' not in portal.objectIds() and 'Notizie' not in portal.objectIds():
+        companies = portal[portal.invokeFactory('Folder','notizie')]
+        companies.setTitle(u'Notizie')
+    if 'il-progetto' not in portal.objectIds():
+        companies = portal[portal.invokeFactory('Folder','il-progetto')]
+        companies.setTitle(u'Il progetto')
+    if 'storie' not in portal.objectIds():
+        companies = portal[portal.invokeFactory('Folder','storie')]
+        companies.setTitle(u'Storie')
+    if 'contatti' not in portal.objectIds():
+        companies = portal[portal.invokeFactory('Folder','contatti')]
+        companies.setTitle(u'Contatti')
 
+    if 'portal_vocabularies' in portal.objectIds():
+        if 'company_type' not in portal.portal_vocabularies.objectIds():
+            company_type = portal.portal_vocabularies[portal.portal_vocabularies.invokeFactory('SortedSimpleVocabulary','company_type')]
+            company_type.setTitle(u'Company type')
+        if 'company_sectore' not in portal.portal_vocabularies.objectIds():
+            company_sectore = portal.portal_vocabularies[portal.portal_vocabularies.invokeFactory('SortedSimpleVocabulary','company_sectore')]
+            company_sectore.setTitle(u'Company sectore')
+        if 'product_category' not in portal.portal_vocabularies.objectIds():
+            product_category = portal.portal_vocabularies[portal.portal_vocabularies.invokeFactory('SortedSimpleVocabulary','product_category')]
+            product_category.setTitle(u'Product category')
+
+
+def upgrade_0000_to_0008(context):
+    portal = context.getSite()
+    setupInitialStructure(portal)
+    add_employee_idx(portal)
+    add_company_idx(portal)
