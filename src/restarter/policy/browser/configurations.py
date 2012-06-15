1ae579c4ca7405d23c8162fa5e988dac8ba76f50
Andrew Mleczko <andrew@mleczko.net>
2012-06-15 10:43:54 +0200

    fix viewlets

--- src/restarter/policy/browser/configurations.py
+++ src/restarter/policy/browser/configurations.py
@@ -3,6 +3,7 @@
 from Products.Five import BrowserView
 from zope.interface import implements
 from zope.component import getMultiAdapter
+from Products.CMFCore.interfaces import ISiteRoot
 
 from restarter.policy.interfaces import IRestarterConfiguration
 
@@ -14,4 +15,7 @@ class RestarterConfig(BrowserView):
     def isHomePage(self):
         context_state = getMultiAdapter((self.context, self.request), name='plone_context_state')
         portal_state = getMultiAdapter((self.context, self.request), name='plone_portal_state')
-        return portal_state.portal_url() == context_state.canonical_object_url()
+        path_check = portal_state.portal_url() == context_state.canonical_object_url()
+        url_check = not ISiteRoot.providedBy(self.context)
+        return path_check and url_check
+
