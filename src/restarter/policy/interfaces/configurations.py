34021b903df2ae72a0495baa2a4563b72345e49d
Andrew Mleczko <andrew@mleczko.net>
2012-06-16 17:10:36 +0200

    extend restarter configuration interface

--- src/restarter/policy/interfaces/configurations.py
+++ src/restarter/policy/interfaces/configurations.py
@@ -1,5 +1,10 @@
 from zope.interface import Interface
 
+
 class IRestarterConfiguration(Interface):
+
     def isHomePage():
         """Return True if we are in homepage"""
+
+    def safe_truncate(text,size):
+        """Return truncated text"""
