b9cdee7db250016250a7040c65c536d4183d5bf1
Andrew Mleczko <andrew@mleczko.net>
2012-06-19 10:44:08 +0200

    fix user personalizations

--- src/restarter/policy/browser/personalpreferences.py
+++ src/restarter/policy/browser/personalpreferences.py
@@ -1,4 +1,5 @@
 from plone.app.users.browser.personalpreferences import UserDataPanel as AccountPanelForm
+from plone.app.users.browser.personalpreferences import UserDataConfiglet as AccountPanelConfiglet
 
 
 class UserDataPanel(AccountPanelForm):
@@ -12,3 +13,12 @@ class UserDataPanel(AccountPanelForm):
         self.form_fields = self.form_fields.omit('accept')
         self.form_fields = self.form_fields.omit('location')
 
+
+class UserDataConfiglet(AccountPanelConfiglet):
+    def __init__(self, context, request):
+        super(UserDataConfiglet, self).__init__(context, request)
+        self.form_fields = self.form_fields.omit('rpx_identifier')
+        self.form_fields = self.form_fields.omit('facebook_id')
+        self.form_fields = self.form_fields.omit('facebook_token')
+        self.form_fields = self.form_fields.omit('accept')
+        self.form_fields = self.form_fields.omit('location')
