23bc5c280dbbb80953895e0d358f1839c2b8d3f6
nekorina <nekorina@redturtle.it>
2012-06-24 11:01:16 +0200

    aggiunto stile responsive

--- src/restarter/policy/upgrades.py
+++ src/restarter/policy/upgrades.py
@@ -127,4 +127,6 @@ def upgrade_0015_to_0016(context):
 def upgrade_0016_to_0017(context):
     context.runAllImportStepsFromProfile('profile-restarter.policy:upgrade_0016_to_0017')
 
+def upgrade_0017_to_0018(context):
+    context.runAllImportStepsFromProfile('profile-restarter.policy:upgrade_0017_to_0018')
 
