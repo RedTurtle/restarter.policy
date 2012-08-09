c041baf519a91fb909088566b450daf17d434437
Andrew Mleczko <andrew@mleczko.net>
2012-08-09 09:38:40 +0200

    add new portlet: other products

---
+++ src/restarter/policy/portlets/other_products.py
@@ -0,0 +1,47 @@
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
+from zope.interface import implements
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from restarter.policy.interfaces import IProduct
+
+
+class IOtherProducts(IPortletDataProvider):
+    """A portlet which shows other product from the same company
+    """
+
+
+class Assignment(base.Assignment):
+    implements(IOtherProducts)
+
+    title = u'Other products'
+
+
+class Renderer(base.Renderer):
+    render = ViewPageTemplateFile('other_products.pt')
+
+    def __init__(self, context, request, view, manager, data):
+        base.Renderer.__init__(self, context, request, view, manager, data)
+
+    def show(self):
+        return IProduct.providedBy(self.context)
+
+    @property
+    def available(self):
+        return self.show()
+
+    def update(self):
+        company = self.context.aq_inner.getCompany()
+        company_path = company.absolute_url_path()
+        self.products = self.context.getFolderContents({'portal_type':'Product',
+                                                        'path': company_path},
+                                                       b_size=10)
+        self.company_title = company.title_or_id()
+        self.products_url = '%s/prodotti' % company.absolute_url()
+
+
+class AddForm(base.NullAddForm):
+
+    def create(self):
+        return Assignment()
+
--- src/restarter/policy/skins/restarter_styles/restarter.js
+++ src/restarter/policy/skins/restarter_styles/restarter.js
@@ -1,3 +1,5 @@
 jQuery(document).ready(function() {
     jq('#companyStoriesList div a').popover({trigger:'hover', title: 'More information', content: 'More information about this story...'})
+    jq('#other-products-portlet').parent().insertAfter($('#portal-logo'));
+    jq('#other-products-portlet').show();
 });
