f319d0f4ae3759f33371a2f34f7a04f69d878ccd
nekorina <nekorina@redturtle.it>
2012-08-09 17:40:32 +0200

    aggiunto popover su portlet other products

--- src/restarter/policy/skins/restarter_styles/restarter.js
+++ src/restarter/policy/skins/restarter_styles/restarter.js
@@ -1,5 +1,11 @@
 jQuery(document).ready(function() {
     jq('#companyStoriesList div a').popover({trigger:'hover', title: 'More information', content: 'More information about this story...'})
+
+    /*** other products ****/
     jq('#other-products-portlet').parent().insertAfter($('#portal-logo'));
     jq('#other-products-portlet').show();
+
+    jq("#other-products-portlet div.productMiniPreview").hover(function(event) {
+        jq(this).popover({trigger:'hover', title: false, position: 'right', classes: 'popoverProductDetail', content: $(this).siblings('.productDetailPreview') });
+    });
 });
