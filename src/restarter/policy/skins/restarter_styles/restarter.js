49113259ef65151d2bc3d5d8e0a101e96b47a8f2
nekorina <nekorina@redturtle.it>
2012-08-10 11:10:46 +0200

    aggiunto facce in home

--- src/restarter/policy/skins/restarter_styles/restarter.js
+++ src/restarter/policy/skins/restarter_styles/restarter.js
@@ -1,5 +1,10 @@
 jQuery(document).ready(function() {
-    jq('#companyStoriesList div a').popover({trigger:'hover', title: 'More information', content: 'More information about this story...'})
+//    jq('#companyStoriesList div a').popover({trigger:'hover', title: 'More information', content: 'More information about this story...'})
+
+    /**** facce in home ***/
+    jq("#faFacce div.unaFaccia").hover(function(event) {
+        jq(this).popover({trigger:'hover', title: false, position: 'right', classes: 'popoverFacciaDetail', content: $(this).children('.facciaPreview') });
+    });
 
     /*** other products ****/
     jq('#other-products-portlet').parent().insertAfter($('#portal-logo'));
